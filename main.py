e37585d2fa06fbbd063f9d99ac683eb768dd7a43
Andrea Brandolini <andreabrandolini15@gmail.com>
2022-06-21 17:00:37 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import data_required
+
 
 #export FLASK_ENv=development
 #export FLASK_APP="file da usare"
@@ -27,6 +31,18 @@ def user(name):
     text = "Hello "+name.capitalize()
     return render_template("user.html",
                            text = text)
+@app.route("/form",methods=["GET","POST"])
+def form():
+
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("Form submitted succesfully")
+    return render_template("Name.html",
+                           name = name,
+                           form = form)
 
 #page not found error
 @app.errorhandler(404)
@@ -37,4 +53,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internalServerError(e):
     return render_template("Error pages/500.html"),500
+
+#create a Form Class
+app.config["SECRET_KEY"] = "tirocinioFBK!"
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name",validators=[data_required()])
+    submit = SubmitField("Submit")
 
